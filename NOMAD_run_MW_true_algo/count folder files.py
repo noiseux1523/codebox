fce524f572c1a715c1076057d210cf4938a12b9d
Loic Sarrazin <sm_loic@hotmail.com>
2017-09-01 14:44:51 -0400

    adding true_mads to run mads with basic parameters

---
+++ NOMAD_run_MW_true_algo/count folder files.py
@@ -0,0 +1,19 @@
+import os, os.path
+
+#print os.listdir('.')
+
+#print len([name for name in os.listdir('.') if os.path.isdir(name)])
+
+
+for i in os.listdir('.'):
+    if os.path.isdir(i):
+        DIR1 = './' + i
+        #print os.listdir(DIR1)
+        dir_list = (j for j in os.listdir(DIR1) if '.' not in j)
+        for k in dir_list:
+            DIR2 = DIR1 + '/' + k
+            ab=os.listdir(DIR2)
+            if (len([j for j in os.listdir(DIR2) if os.path.isfile(os.path.join(DIR2, j))])) == 40:
+                print DIR2
+
+        #print str(i) + " is a directory with " + str(len([j for j in os.listdir('./'+i) if os.path.isdir(os.path.join(DIR,j))]))
